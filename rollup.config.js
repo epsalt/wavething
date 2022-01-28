fb5d339af33349ff9ab0c673298e904507b7ce19
Evan Saltman <evan.saltman@gmail.com>
2022-01-28 10:59:34 -0700

    initial commit

---
+++ rollup.config.js
@@ -0,0 +1,32 @@
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import babel from "@rollup/plugin-babel";
+import commonjs from "@rollup/plugin-commonjs";
+import copy from "rollup-plugin-copy";
+import replace from "@rollup/plugin-replace";
+
+export default {
+  input: "src/index.js",
+  output: {
+    file: "dist/bundle.js",
+    format: "cjs",
+  },
+  plugins: [
+    nodeResolve({
+      browser: true,
+    }),
+    replace({
+      "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV),
+    }),
+    babel({
+      exclude: "node_modules/**",
+      presets: ["@babel/env", "@babel/preset-react"],
+    }),
+    commonjs(),
+    copy({
+      targets: [
+        { src: "public/index.html", dest: "dist" },
+        { src: "data", dest: "dist" },
+      ],
+    }),
+  ],
+};
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Waveform from "./components/Waveform";
+
+const App = () => {
+  return (
+    <div>
+      <Waveform />
+    </div>
+  );
+};
+
+export default App;
