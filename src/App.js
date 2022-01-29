118771c569a7d1c5503b42463a2be99f0c18c44e
Evan Saltman <evan.saltman@gmail.com>
2022-01-29 13:11:12 -0700

    Add bar count and bar spacing sliders

--- src/App.js
+++ src/App.js
@@ -3,9 +3,15 @@ import Waveform from "./components/Waveform";
 import WaveformData from "waveform-data";
 
 const App = () => {
+  const [barWidth, setBarWidth] = useState(0.8);
+  const [barSpacing, setBarSpacing] = useState(0.5);
   const [selectedFile, setSelectedFile] = useState();
   const [audioData, setAudioData] = useState();
   const [error, setError] = useState();
+
+  useEffect(() => {
+    loadAudio();
+  }, [selectedFile]);
 
   const loadAudio = async () => {
     const audio = selectedFile ? selectedFile : await fetch("data/chirp.mp3");
@@ -28,22 +34,44 @@ const App = () => {
     });
   };
 
-  useEffect(() => {
-    loadAudio();
-  }, [selectedFile]);
-
-  const changeHandler = (event) => {
+  const fileChangeHandler = (event) => {
     setSelectedFile(event.target.files[0]);
   };
 
   return (
     <div>
       <p>
-        <input type="file" name="file" onChange={changeHandler} />
+        <input type="file" name="file" onChange={fileChangeHandler} />
+      </p>
+      <p>
+        <input
+          type="range"
+          min="0"
+          max="0.9"
+          step="0.1"
+          value={barWidth}
+          onChange={(event) => setBarWidth(parseFloat(event.target.value))}
+        />
+        Bar Count
+      </p>
+      <p>
+        <input
+          type="range"
+          min="0"
+          max="0.9"
+          step="0.1"
+          value={barSpacing}
+          onChange={(event) => setBarSpacing(parseFloat(event.target.value))}
+        />
+        Bar Spacing
       </p>
       {error && <p>{error}</p>}
       <div>
-        <Waveform audioData={audioData} />
+        <Waveform
+          audioData={audioData}
+          barSpacing={barSpacing}
+          barWidth={barWidth}
+        />
       </div>
     </div>
   );
--- src/components/Waveform.js
+++ src/components/Waveform.js
@@ -3,7 +3,7 @@ import { select } from "d3-selection";
 import { scaleLinear } from "d3-scale";
 import React, { useEffect, useRef } from "react";
 
-const Waveform = ({ audioData }) => {
+const Waveform = ({ audioData, barSpacing, barWidth }) => {
   const ref = useRef();
 
   useEffect(() => {
@@ -14,22 +14,23 @@ const Waveform = ({ audioData }) => {
       return;
     }
 
-    const waveform = audioData.resample({ width: 300 });
+    const waveform = audioData.resample({ width: 512 * (1 - barWidth) });
     const channel = waveform.channel(0);
-
     const minChannel = channel.min_array();
     const maxChannel = channel.max_array();
 
     const width = 500;
     const height = 500;
-    const padding = 1;
-    const bandwidth = width / maxChannel.length - padding;
+
+    const step = width / maxChannel.length;
+    const padding = step * barSpacing;
+    const bandwidth = step - padding;
 
     const x = scaleLinear().domain([0, waveform.length]).range([0, width]);
 
     const y = scaleLinear()
       .domain([min(minChannel), max(maxChannel)])
-      .rangeRound([height / 2, -height / 2]);
+      .range([height / 2, -height / 2]);
 
     svg.attr("width", width).attr("height", height);
 
@@ -37,6 +38,7 @@ const Waveform = ({ audioData }) => {
       .append("g")
       .attr("transform", () => `translate(0, ${height / 2})`)
       .attr("fill", "blue")
+      .attr("stroke", "none")
       .selectAll("rect")
       .data(maxChannel)
       .join("rect")
@@ -44,7 +46,7 @@ const Waveform = ({ audioData }) => {
       .attr("y", (_, i) => y(maxChannel[i]))
       .attr("height", (d, i) => Math.max(y(minChannel[i]) - y(d), y(0) - y(1)))
       .attr("width", bandwidth);
-  }, [audioData]);
+  }, [audioData, barSpacing, barWidth]);
 
   return <svg ref={ref} />;
 };
